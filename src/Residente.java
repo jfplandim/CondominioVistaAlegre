3a70d0a6993ef3faa78c6c8fa53902f257fbed74
yuri <yuridouglas45@gmail.com>
2025-11-01 17:17:15 -0300

    atributo id

--- src/Residente.java
+++ src/Residente.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.time.LocalDateTime;
 public class Residente {
+    private int id;
     private String nome;
     private int telefone;
     private String email;
