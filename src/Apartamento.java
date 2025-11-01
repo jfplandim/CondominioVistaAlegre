1dba4684c6aa478663d0148bb891bf4090e4f94f
Jose Landim <joselandimsobrinho@hotmail.com>
2025-11-01 17:28:10 -0300

    Ajustes da classe Apartamento

--- src/Apartamento.java
+++ src/Apartamento.java
@@ -4,34 +4,70 @@ import java.util.List;
 //representa uma unidade de apartamento
 
 public class Apartamento {
+
     private String numero;
-    private int andar;
     private String bloco;
-    private int vagasGaragem; // Requisito: "número de vagas de garagem"
-    private List<Residente> residentes;
+    private int numeroVagas;
+    private Residente titular;
+    private List<Residente> moradores;
+    private double saldoCondominio;
 
-    public Apartamento(String numero, int andar, String bloco, int vagasGaragem) {
+    public Apartamento(String numero, String bloco, int numeroVagas, Residente titular, double saldoCondominio) {
+
         this.numero = numero;
-        this.andar = andar;
         this.bloco = bloco;
-        this.vagasGaragem = vagasGaragem;
-        this.residentes = new ArrayList<>();
+        this.numeroVagas = numeroVagas;
+        this.moradores = new ArrayList<>();
+        this.saldoCondominio = saldoCondominio;
     }
 
-    public void adicionarMorador(Residente residente) {
-        this.residentes.add(residente);
-    }
 
-    // Getters e Setters
+
     public String getNumero() {
         return numero;
     }
 
-    public int getVagasGaragem() {
-        return vagasGaragem; //
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
-    public List<Residente> getResidentes() {
-        return residentes;
+    public int getNumeroVagas() {
+        return numeroVagas;
+    }
+
+    public void setNumeroVagas(int numeroVagas) {
+        this.numeroVagas = numeroVagas;
+    }
+
+    public String getBloco() {
+        return bloco;
+    }
+
+    public void setBloco(String bloco) {
+        this.bloco = bloco;
+    }
+
+    public Residente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Residente titular) {
+        this.titular = titular;
+    }
+
+    public List<Residente> getMoradores() {
+        return moradores;
+    }
+
+    public void setMoradores(List<Residente> moradores) {
+        this.moradores = moradores;
+    }
+
+    public double getSaldoCondominio() {
+        return saldoCondominio;
+    }
+
+    public void setSaldoCondominio(double saldoCondominio) {
+        this.saldoCondominio = saldoCondominio;
     }
 }
