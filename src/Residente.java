2ff3367e4f220dbcb98d26188fbb3abe7dc0d442
yuri <yuridouglas45@gmail.com>
2025-11-01 17:14:44 -0300

    Classe residente, com os atributos, as listas de pets, vagas e a lista de visitantes. também a solicitação de manutenção e fazer reserva

---
+++ src/Residente.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.time.LocalDateTime;
+public class Residente {
+    private String nome;
+    private int telefone;
+    private String email;
+
+    private List<Pet> pets;
+    private List<vagaGaragem> vagaGaragens;
+    private List<Visitante> visitantesAutorizados;
+
+    public void login(){
+        System.out.println("Morador fazendo login...");
+    }
+
+    public void solicitacaoManutencao(int areaId, String descricao){
+        System.out.println("solicitando manutanção para a área" + areaId);
+    }
+    public void fazerReserva(int areaID, LocalDateTime start, LocalDateTime end){
+        System.out.println("Fazendo reserva" + areaID);
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public List<vagaGaragem> getVagaGaragens() {
+        return vagaGaragens;
+    }
+
+    public void setVagaGaragens(List<vagaGaragem> vagaGaragens) {
+        this.vagaGaragens = vagaGaragens;
+    }
+
+    public List<Visitante> getVisitantesAutorizados() {
+        return visitantesAutorizados;
+    }
+
+    public void setVisitantesAutorizados(List<Visitante> visitantesAutorizados) {
+        this.visitantesAutorizados = visitantesAutorizados;
+    }
+}
